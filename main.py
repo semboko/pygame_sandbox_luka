21ee50762a4b2a10d8fc07393d109cae7d09824d
Simon Bondarenko <bondarenko@vt.edu>
2023-04-11 14:19:31 -0600

    Added class Game, refactored main.py

--- main.py
+++ main.py
@@ -1,56 +1,41 @@
 import pygame
 import pymunk
-from components.floor import Floor
-from components.catapult import Catapult
+from typing import Type
+from scenes.abstract import BaseScene
 
-pygame.init()
-display = pygame.display.set_mode((500, 500))
-h = display.get_height()
-clock = pygame.time.Clock()
 
 space = pymunk.Space()
 space.gravity = (0, -1000)
 
-floor = Floor(space)
-catapult = Catapult(250, 300, 75, space)
-bullets = []
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_r:
-                catapult.reload()
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.display = pygame.display.set_mode((500, 500))
+        self.clock = pygame.time.Clock()
+        self.scene = None
+    
+    def load_scene(self, Scene: Type[BaseScene]):
+        self.scene = Scene()
+    
+    def run(self) -> None:
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                self.scene.hadnle_event(event)
             
-            if event.key == pygame.K_SPACE:
-                catapult.shot()
+            pk = pygame.key.get_pressed()
+            self.scene.handle_pressed_keys(pk)
+            
+            self.scene.update()
+            
+            self.scene.render(self.display)
+            
+            pygame.display.update()
+            self.clock.tick(60)
     
-    pressed_keys = pygame.key.get_pressed()
-    if pressed_keys[pygame.K_LEFT]:
-        catapult.move_arm()
-    
-    if pressed_keys[pygame.K_a]:
-        catapult.move(-1)
-    
-    if pressed_keys[pygame.K_d]:
-        catapult.move(1)
-        
-    ammo = catapult.detach_ammo()
-    if ammo:
-        bullets.append(ammo)
 
-    catapult.update_motor_rate()
-    
-    display.fill((255, 255, 255))
-
-    floor.render(display)
-    catapult.render(display)
-    for b in bullets:
-        b.render(display)
-    
-    pygame.display.update()
-    space.step(1/60)
-    clock.tick(60)
+g = Game()
+g.load_scene(BaseScene)  # !!! Change to the concrete scene
+g.run()
