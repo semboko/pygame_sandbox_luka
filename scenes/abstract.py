7f32387ce194fbff3f908cc7f2a587468a65c616
Simon Bondarenko <bondarenko@vt.edu>
2023-04-11 14:01:22 -0600

    Added an abstract scene

---
+++ scenes/abstract.py
@@ -0,0 +1,20 @@
+from pygame.event import Event
+from typing import Sequence
+from pygame.surface import Surface
+
+
+class BaseScene:
+    def __init__(self) -> None:
+        pass
+    
+    def hadnle_event(self, event: Event) -> None:
+        pass
+    
+    def handle_pressed_keys(self, keys: Sequence[bool]) -> None:
+        pass
+    
+    def update(self) -> None:
+        pass
+    
+    def render(self, display: Surface) -> None:
+        pass
