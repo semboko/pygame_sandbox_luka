0711d78ffdc36d4aa010d3c6da78c3986477d529
Simon Bondarenko <bondarenko@vt.edu>
2023-04-25 13:47:30 -0600

    Added the image for tank base

--- components/tank.py
+++ components/tank.py
@@ -1,5 +1,5 @@
 from pymunk import Vec2d, Body, Poly, Space
-from pygame import Surface, draw
+from pygame import Surface, draw, image
 from utils import convert
 
 
@@ -9,8 +9,8 @@ class TankBase:
         self.origin = origin
         
         self.body = Body()
-        center = Vec2d(85, -15)
-        self.body.position = origin + center
+        self.center = Vec2d(85, -15)
+        self.body.position = origin + self.center
         
         points = (
             (0, 0),
@@ -25,21 +25,25 @@ class TankBase:
         )
         
         self.shape = Poly(self.body, [
-            convert(Vec2d(*p) - center, 32)
+            convert(Vec2d(*p) - self.center, 32)
             for p in points
         ])
         self.shape.density = 1
         space.add(self.body, self.shape)
         
+        self.image = image.load("./assets/tank_base.png", "png")
+        
     
     def render(self, display: Surface) -> None:
         h = display.get_height()
+        
         points = [
             convert(self.body.local_to_world(v), h)
             for v in self.shape.get_vertices()
         ]
         
-        draw.polygon(display, (255, 0, 150), points)
+        display.blit(self.image, convert(self.body.position - self.center, h))
+        draw.polygon(display, (255, 0, 150), points, 1)
 
 
 class Tank:
